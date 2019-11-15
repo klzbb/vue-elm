000b2a95ba356a644f4423326c12d7fbc9c9abc8
konglingzhan <1397424822@qq.com>
2019-11-15 23:42:48 +0800

    init

---
+++ .env.pro
@@ -0,0 +1,5 @@
+# 测试环境
+NODE_ENV = 'production'
+
+# base url
+VUE_APP_BASEURL = 'http://baidu.com'
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://m.baidu.com', // 目标地址
+        ws: true, /// / 是否启用websockets
+        changeOrigin: true, // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: { '^/api': '/' } // 这里重写路径
+      }
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          // 把px单位换算成rem单位
+          require('postcss-pxtorem')({
+            rootValue: 75, // 换算的基数(设计图750的根字体为32)
+            selectorBlackList: ['.van'], // 要忽略的选择器并保留为px。
+            propList: ['*'], // 可以从px更改为rem的属性。
+            minPixelValue: 2 // 设置要替换的最小像素值。
+          })
+        ]
+      }
+    }
+  }
+}
