1d99ee226a83dc7eec8dcd8baa514c2a4ee24d79
konglingzhan_zt <1397424822@qq.com>
2019-12-06 10:57:18 +0800

    fastclick配置

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
+/*
+ * @Descripttion: DOP
+ * @version: 1.0.0
+ * @Author: Author
+ * @Date: 2019-11-26 17:45:15
+ * @LastEditors: konglingzhan
+ * @LastEditTime: 2019-12-06 10:54:18
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FastClick from 'fastclick'
+
 import 'lib-flexible/flexible.js' // rem set
 import '@/icons' // global svg
-import '@/permission.js' // permission control
+import '@/permission.js'
+FastClick.attach(document.body) // permission control
 
 Vue.config.productionTip = false
 
