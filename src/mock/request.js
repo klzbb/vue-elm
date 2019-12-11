7a8f52e8849f1aa5684605c9d066429127a7c126
konglingzhan_zt <1397424822@qq.com>
2019-12-11 17:11:31 +0800

    bug修复

--- src/mock/request.js
+++ src/mock/request.js
@@ -4,11 +4,11 @@
  * @Author: Author
  * @Date: 2019-07-10 09:03:03
  * @LastEditors: konglingzhan
- * @LastEditTime: 2019-12-05 09:27:23
+ * @LastEditTime: 2019-12-11 17:02:02
  */
 import axios from 'axios'
-import { Toast } from 'vant';
-import router from '@/router/router.js'
+// import { Toast } from 'vant'
+import router from '@/router/index.js'
 import store from '@/store/index.js'
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API,
@@ -19,9 +19,9 @@ const service = axios.create({
 
 // request interceptor
 service.interceptors.request.use(function (config) {
-  return config;
+  return config
 }, function (error) {
-  return Promise.reject(error);
+  return Promise.reject(error)
 })
 
 // response interceptor
@@ -29,37 +29,37 @@ service.interceptors.response.use(function (response) {
   /**
    * 200      success
    */
-  let condition = [200];
+  let condition = [200]
   let isPass = condition.findIndex(item => item === response.data.code)
   if (isPass !== -1) {
-    return response;
+    return response
   } else {
     // test(response);
     // 接口code没有成功情况先关闭加载框再弹出异常提示
     store.commit('dialog/SET_LOADING_SHOW', 0)
-    Toast({
-      type: 'fail',
-      message: response.data.msg
-    })
+    // Toast({
+    //   type: 'fail',
+    //   message: response.data.msg
+    // })
   }
 }, function (error) {
   // service error : 404,400,500 handler
   store.commit('dialog/SET_LOADING_SHOW', 0)
   switch (error.response.status) {
     case 404:
-      router.push('/404');
-      break;
+      router.push('/404')
+      break
     case 401:
-      router.push('/login');
-      break;
+      router.push('/login')
+      break
     case 500:
     case 501:
-      router.push('/50X');
-      break;
+      router.push('/50X')
+      break
     default:
-      break;
+      break
   }
-  return Promise.reject(error);
+  return Promise.reject(error)
 })
 
 export default service
